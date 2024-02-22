c27b1c22077baea9dc273b77a80cc04ad54975e3
Xander <xanderpalmer7@gmail.com>
2024-02-22 11:30:49 -0500

    Initialized hardhat project

---
+++ backend/hardhat.config.js
@@ -0,0 +1,15 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+const SEPOLIA_API_KEY = vars.get("SEPOLIA_API_KEY");
+const BET_OWNER_PRIVATE_KEY = vars.get("BET_OWNER_PRIVATE_KEY");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.24",
+  networks: {
+    sepolia: {
+      url: 'https://eth-sepolia.g.alchemy.com/v2/' + SEPOLIA_API_KEY,
+      accounts: [BET_OWNER_PRIVATE_KEY]
+    }
+  },
+};
