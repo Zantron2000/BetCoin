514095d20d3cc9eeb0264947a0609f2266bb6edb
Xander <xanderpalmer7@gmail.com>
2024-03-12 10:59:13 -0400

    Documented Alchemy tools

--- frontend/src/utils/Alchemy.js
+++ frontend/src/utils/Alchemy.js
@@ -1,9 +1,22 @@
 import axios from 'axios';
 
+// The alchemy route for getting the NFTs for a specific owner
 const getNFTsForOwnerRoute = `https://eth-sepolia.g.alchemy.com/nft/v3/${import.meta.env.VITE_ALCHEMY_API_KEY}/getNFTsForOwner?owner=$owner&contractAddresses[]=${import.meta.env.VITE_BET_TOKEN_CONTRACT_ADDRESS}&withMetadata=false`;
 
+// The alchemy route for getting the owners for a specific contract
 const getOwnersForContractRoute = `https://eth-sepolia.g.alchemy.com/nft/v3/${import.meta.env.VITE_ALCHEMY_API_KEY}/getOwnersForContract?contractAddress=${import.meta.env.VITE_BET_TOKEN_CONTRACT_ADDRESS}&withTokenBalances=true`
 
+/**
+ * Get the BetToken NFTs for a specific owner
+ *
+ * @param {String} ownerAddress The address of the owner
+ * @returns {Promise<{
+ *   totalCount: Number,
+*   nfts: {
+*     tokenId: Number
+*   }[]
+* }?>} The NFTs for the owner or null if there was an error
+ */
 export const getNFTsForOwner = async (ownerAddress) => {
     try {
         const { data } = await axios.get(getNFTsForOwnerRoute.replace('$owner', ownerAddress));
@@ -19,6 +32,14 @@ export const getNFTsForOwner = async (ownerAddress) => {
     }
 }
 
+/**
+ * Gets all NFTs and Owners for the BetToken contract
+ *
+ * @returns {Promise<{
+ *   tokenId: Number,
+*   owner: String
+* }[]?>} The owners of the NFT contract or null if there was an error
+ */
 export const getOwnersForContract = async () => {
     try {
         const { data } = await axios.get(getOwnersForContractRoute);
