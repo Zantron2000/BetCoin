dad32487229c6895ccb1b026c933f9030416d2e1
Xander <xanderpalmer7@gmail.com>
2024-03-12 10:59:36 -0400

    Documented BetCoin tools

--- frontend/src/utils/BetCoin.js
+++ frontend/src/utils/BetCoin.js
@@ -1,3 +1,13 @@
+/**
+ * Gets the account info for a specific address in the BetCoin contract
+ *
+ * @param {Object} contract The viem contract instance of the BetCoin contract
+ * @param {String} address The address of the account to get the info for
+ * @returns {Promise<{
+ *   balance: String,
+ *   unlock: String
+ * }?>} The account info or null if there was an error
+ */
 export const getAccountInfo = async (contract, address) => {
     try {
         const [balance, unlock] = await Promise.all([
@@ -16,6 +26,12 @@ export const getAccountInfo = async (contract, address) => {
     }
 }
 
+/**
+ * Starts a transaction to mint coins from the BetCoin contract
+ *
+ * @param {Object} contract The viem contract instance of the BetCoin contract
+ * @returns {Promise<String?>} The transaction hash of the mint or null if there was an error
+ */
 export const mint = async (contract) => {
     try {
         const transactionHash = await contract.write.mint();
