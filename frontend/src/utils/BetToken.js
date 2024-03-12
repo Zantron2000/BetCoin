51d90bba9650d926f246b3135d7b4a079763796c
Xander <xanderpalmer7@gmail.com>
2024-03-12 10:59:25 -0400

    Documented BetToken tools

--- frontend/src/utils/BetToken.js
+++ frontend/src/utils/BetToken.js
@@ -1,3 +1,12 @@
+/**
+ * Gets the account info for a specific address for the BetToken contract
+ * 
+ * @param {Object} contract The viem contract instance of the BetToken contract
+ * @param {String} address The address of the account to get the info for
+ * @returns {Promise<{
+ *   balance: Number
+* }?>} The account info or null if there was an error
+ */
 export const getAccountInfo = async (contract, address) => {
     try {
         const [balance] = await Promise.all([
@@ -14,6 +23,15 @@ export const getAccountInfo = async (contract, address) => {
     }
 }
 
+/**
+ * Gets the token info for a specific NFT
+ *
+ * @param {Object} contract The viem contract instance of the BetToken contract
+ * @param {String} tokenId The id of the token to get the info for
+ * @returns {Promise<{
+ *   owner: String
+* }?>} The token info or null if there was an error
+ */
 export const getTokenInfo = async (contract, tokenId) => {
     try {
         const [owner] = await Promise.all([
@@ -30,6 +48,13 @@ export const getTokenInfo = async (contract, tokenId) => {
     }
 }
 
+/**
+ * Starts a transaction to mint an NFT for a specific address
+ * 
+ * @param {Object} contract The viem contract instance of the BetToken contract
+ * @param {String} address The address of the account to mint the token for
+ * @returns {Promise<String?>} The transaction hash of the mint or null if there was an error
+ */
 export const mint = async (contract, address) => {
     try {
         const transactionHash = await contract.write.mint([address]);
